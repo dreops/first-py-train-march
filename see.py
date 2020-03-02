ab307b026a7f25440f0c49e4e65df589873e7838
dreops <dreops@andreasandrews.com>
2020-03-02 23:03:36 +0000

    attempt 2 of updating changes to see.py from home

--- see.py
+++ see.py
@@ -1,12 +1,19 @@
 
+#SEE - Solo Online Exercise. Task: How many times 'word' appear in phrase
+
+#Some example inputs, used for testing.
 word = "hi"
 phrase = "hi hiya hello hi"
 
-def SEE1(word, phrase):
+#Below is the starting line provided
+def SEE1(word, phrase): # Here the function is defined. Below is my solution
     string1 = word
-    string2 = phrase.split (" ")
-    for i in range([string2]):
-        i = i + 1
+    string2 = phrase
+    SEE1 = string1
+    #string2 = phrase.split (" ")
+    #for i in range([string2]):
+    for i in range(len(string2)):
+        i = wordsphrase
     return SEE1
 
 print(SEE1(word, phrase))
